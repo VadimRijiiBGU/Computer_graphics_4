d0f93981679c50abd65c0c71d4f57b9a560424d0
VadimRijiiBGU <nikolskivadim@gmail.com>
2020-04-26 00:23:01 +0300

    feat: done lab 4

---
+++ Rasterization/Form1.Designer.cs
@@ -0,0 +1,258 @@
+﻿namespace Rasterization
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripDropDownButton1 = new System.Windows.Forms.ToolStripDropDownButton();
+            this.button3 = new System.Windows.Forms.ToolStripMenuItem();
+            this.button4 = new System.Windows.Forms.ToolStripMenuItem();
+            this.button5 = new System.Windows.Forms.ToolStripMenuItem();
+            this.button1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.button2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.label6 = new System.Windows.Forms.ToolStripLabel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(2, 29);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(987, 603);
+            this.pictureBox1.TabIndex = 10;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripDropDownButton1,
+            this.label6});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(1063, 27);
+            this.toolStrip1.TabIndex = 18;
+            this.toolStrip1.Text = "toolStrip1";
+            this.toolStrip1.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.toolStrip1_ItemClicked);
+            // 
+            // toolStripDropDownButton1
+            // 
+            this.toolStripDropDownButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripDropDownButton1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.button3,
+            this.button4,
+            this.button5,
+            this.button1,
+            this.button2});
+            this.toolStripDropDownButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripDropDownButton1.Image")));
+            this.toolStripDropDownButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripDropDownButton1.Name = "toolStripDropDownButton1";
+            this.toolStripDropDownButton1.Size = new System.Drawing.Size(33, 24);
+            this.toolStripDropDownButton1.Text = "toolStripDropDownButton1";
+            // 
+            // button3
+            // 
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(158, 22);
+            this.button3.Text = "Cda";
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(158, 22);
+            this.button4.Text = "BrezinhamLine";
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(158, 22);
+            this.button5.Text = "Brezinhamcircle";
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button1
+            // 
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(158, 22);
+            this.button1.Text = "Steps";
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(158, 22);
+            this.button2.Text = "ClearWindow";
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label6
+            // 
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(0, 24);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(1033, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 13);
+            this.label1.TabIndex = 19;
+            this.label1.Text = "Start Point";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(994, 65);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(53, 20);
+            this.textBox1.TabIndex = 20;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(1070, 65);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(53, 20);
+            this.textBox2.TabIndex = 21;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(1033, 112);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 22;
+            this.label2.Text = "End Point";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(994, 162);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(53, 20);
+            this.textBox3.TabIndex = 23;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(1070, 162);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(53, 20);
+            this.textBox4.TabIndex = 24;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(1033, 219);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(40, 13);
+            this.label3.TabIndex = 25;
+            this.label3.Text = "Radius";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(1033, 259);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(53, 20);
+            this.textBox5.TabIndex = 26;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(994, 310);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(30, 13);
+            this.label4.TabIndex = 27;
+            this.label4.Text = "Time";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(1138, 636);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.pictureBox1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.MaximumSize = new System.Drawing.Size(1442, 843);
+            this.MinimumSize = new System.Drawing.Size(1154, 675);
+            this.Name = "Form1";
+            this.Padding = new System.Windows.Forms.Padding(0, 0, 75, 0);
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripDropDownButton toolStripDropDownButton1;
+        private System.Windows.Forms.ToolStripMenuItem button3;
+        private System.Windows.Forms.ToolStripMenuItem button4;
+        private System.Windows.Forms.ToolStripMenuItem button5;
+        private System.Windows.Forms.ToolStripMenuItem button1;
+        private System.Windows.Forms.ToolStripMenuItem button2;
+        private System.Windows.Forms.ToolStripLabel label6;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Rasterization/Form1.cs
@@ -0,0 +1,315 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Rasterization
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private static int scale = 1;
+        int x1, x2, y1, y2, r;
+        int padding = 30;
+        Tuple<int, int> focusPoint = new Tuple<int, int>(0,0);
+        private static HashSet<Tuple<int, int>> points = new HashSet<Tuple<int, int>>();
+
+        private void changeTime(long time)
+        {
+            label4.Text = $"Time: {time} ticks";
+        }
+
+        private void Swap(ref int x1, ref int x2)
+        {
+            int t = x2;
+            x2 = x1;
+            x1 = t;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Stopwatch clock = new Stopwatch();
+            clock.Start();
+
+            x1 = int.Parse(textBox1.Text);
+            x2 = int.Parse(textBox3.Text);
+            y1 = int.Parse(textBox2.Text);
+            y2 = int.Parse(textBox4.Text);
+
+            int startY = Math.Min(y1, y2);
+            int endY = Math.Max(y1, y2);
+
+            if (x1 == x2)
+            {
+                for (int i = startY; i <= endY; i++)
+                {
+                    points.Add(new Tuple<int, int>(x1, i));
+                }
+            }
+            else
+            {
+                int startX = Math.Min(x1, x2);
+                int endX = Math.Max(x1, x2);
+
+                double k = (y1 - y2) * 1.0 / (x1 - x2);
+                double b = y1 - k * x1;
+                if (Math.Abs(k) >= 1)
+                {
+                    for (int i = startY; i <= endY; i++)
+                    {
+                        points.Add(new Tuple<int, int>((int)Math.Round((i - b) / k), i));
+                    }
+                }
+                else
+                {
+                    for (int i = startX; i <= endX; i++)
+                    {
+                        points.Add(new Tuple<int, int>(i, (int)Math.Round(k * i + b)));
+                    }
+                }
+            }
+
+            clock.Stop();
+            changeTime(clock.ElapsedTicks);
+            pictureBox1.Refresh();
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+
+            int startX = -1000 / (scale * 2) + focusPoint.Item1;
+            int startY = -600 / (scale * 2) + focusPoint.Item2;
+            int endX = startX + 1000 / scale;
+            int endY = startY + 600 / scale;
+
+            for (int i = startX; i <= endX; i += (endX - startX) / 40)
+            {
+                int newX = (i - startX) * scale + padding;
+                e.Graphics.DrawString(i.ToString(), new Font("Arial", 10), new SolidBrush(Color.Black), newX - 5, 0);
+                e.Graphics.DrawLine(new Pen(Brushes.Black), new Point(newX, padding), new Point(newX, 600 + padding));
+            }
+
+            for (int i = startY; i <= endY; i += (endY - startY) / 20)
+            {
+                int newY = (i - startY) * scale + padding;
+                e.Graphics.DrawString(i.ToString(), new Font("Arial", 10), new SolidBrush(Color.Black), 0, newY - 5);
+                e.Graphics.DrawLine(new Pen(Brushes.Black), new Point(padding, newY), new Point(1000 + padding, newY));
+            }
+
+            if (startX <= 0 && endX >= 0)
+            {
+                int newX = -startX * scale + padding;
+                e.Graphics.DrawLine(new Pen(Brushes.Blue), new Point(newX, padding), new Point(newX, 600 + padding));
+            }
+
+            if (startY <= 0 && endY >= 0)
+            {
+                int newY = -startY * scale + padding;
+                e.Graphics.DrawLine(new Pen(Brushes.Blue), new Point(padding, newY), new Point(1000 + padding, newY));
+            }
+
+            drawAllPoints(e.Graphics);
+        }
+
+        private void drawAllPoints(Graphics g)
+        {
+            int frameLeftX = focusPoint.Item1 - 500 / scale;
+            int frameRightX = focusPoint.Item1 + 500 / scale;
+            int frameLeftY = focusPoint.Item2 - 300 / scale;
+            int frameRightY = focusPoint.Item2 + 300 / scale;
+
+            foreach (Tuple<int, int> tuple in points)
+            {
+                if (tuple.Item1 >= frameLeftX && tuple.Item1 <= frameRightX && tuple.Item2 >= frameLeftY && tuple.Item2 <= frameRightY)
+                {
+                    g.FillRectangle(Brushes.Green, (tuple.Item1 - frameLeftX) * scale + padding, (tuple.Item2 - frameLeftY) * scale + padding, scale, scale);
+                }
+            }
+        }
+
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            int realX = (e.X - 530) / scale;
+            int realY = (e.Y - 400) / scale;
+
+            if (e.Button == MouseButtons.Right)
+            {
+                if (scale != 1)
+                {
+                    scale /= 5;
+                    focusPoint = new Tuple<int, int>(focusPoint.Item1 + realX, focusPoint.Item2 + realY);
+                }
+            }
+            else
+            {
+                if (scale < 25)
+                {
+                    scale *= 5;
+                    focusPoint = new Tuple<int, int>(focusPoint.Item1 + realX, focusPoint.Item2 + realY);
+                }
+            }
+
+            if (scale == 1) focusPoint = new Tuple<int, int>(0, 0);
+
+            pictureBox1.Refresh();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            points.Clear();
+            pictureBox1.Refresh();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            x1 = int.Parse(textBox1.Text);
+            x2 = int.Parse(textBox3.Text);
+            y1 = int.Parse(textBox2.Text);
+            y2 = int.Parse(textBox4.Text);
+
+            int L = Math.Max(Math.Abs(x2 - x1), Math.Abs(y2 - y1));
+            double dX = (x2 - x1) * 1.0 / L;
+            double dY = (y2 - y1) * 1.0 / L;
+            points.Add(new Tuple<int, int>(x1, y1));
+            double prevX = x1;
+            double prevY = y1;
+            int i = 1;
+            while (i < L)
+            {
+                prevX += dX;
+                prevY += dY;
+                points.Add(new Tuple<int, int>((int)Math.Round(prevX), (int)Math.Round(prevY)));
+                i++;
+            }
+
+            points.Add(new Tuple<int, int>(x2, y2));
+            stopwatch.Stop();
+            changeTime(stopwatch.ElapsedTicks);
+            pictureBox1.Refresh();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            x1 = int.Parse(textBox1.Text);
+            x2 = int.Parse(textBox3.Text);
+            y1 = int.Parse(textBox2.Text);
+            y2 = int.Parse(textBox4.Text);
+            var steep = Math.Abs(y2 - y1) > Math.Abs(x2 - x1);
+            if (steep)
+            {
+                Swap(ref x1, ref y1);
+                Swap(ref x2, ref y2);
+            }
+
+            if (x1 > x2)
+            {
+                Swap(ref x1, ref x2);
+                Swap(ref y1, ref y2);
+            }
+
+            int dx = Math.Abs(x2 - x1);
+            int dy = Math.Abs(y2 - y1);
+            int error = dx / 2;
+            int ystep = (y1 < y2) ? 1 : -1;
+            int y = y1;
+            for (int x = x1; x <= x2; x++)
+            {
+                points.Add(new Tuple<int, int>(steep ? y : x, steep ? x : y));
+                error -= dy;
+                if (error < 0)
+                {
+                    y += ystep;
+                    error += dx;
+                }
+            }
+
+            stopwatch.Stop();
+            changeTime(stopwatch.ElapsedTicks);
+            pictureBox1.Refresh();
+        }
+
+        private void toolStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs ea)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            x1 = int.Parse(textBox1.Text);
+            y1 = int.Parse(textBox2.Text);
+            r = int.Parse(textBox5.Text);
+            int x = 0;
+            int y = r;
+            int e = 3 - 2 * r;
+            points.Add(new Tuple<int, int>(x + x1, y + y1));
+            points.Add(new Tuple<int, int>(x + x1, -y + y1));
+            points.Add(new Tuple<int, int>(-x + x1, y + y1));
+            points.Add(new Tuple<int, int>(-x + x1, -y + y1));
+
+            points.Add(new Tuple<int, int>(y + x1, x + y1));
+            points.Add(new Tuple<int, int>(-y + x1, x + y1));
+            points.Add(new Tuple<int, int>(y + x1, -x + y1));
+            points.Add(new Tuple<int, int>(-y + x1, -x + y1));
+            while (x < y)
+            {
+                if (e >= 0)
+                {
+                    e = e + 4 * (x - y) + 10;
+                    x = x + 1;
+                    y = y - 1;
+                }
+                else
+                {
+                    e = e + 4 * x + 6;
+                    x = x + 1;
+                }
+
+                points.Add(new Tuple<int, int>(x + x1, y + y1));
+                points.Add(new Tuple<int, int>(x + x1, -y + y1));
+                points.Add(new Tuple<int, int>(-x + x1, y + y1));
+                points.Add(new Tuple<int, int>(-x + x1, -y + y1));
+
+                points.Add(new Tuple<int, int>(y + x1, x + y1));
+                points.Add(new Tuple<int, int>(-y + x1, x + y1));
+                points.Add(new Tuple<int, int>(y + x1, -x + y1));
+                points.Add(new Tuple<int, int>(-y + x1, -x + y1));
+
+                
+            }
+            stopwatch.Stop();
+            changeTime(stopwatch.ElapsedTicks);
+            pictureBox1.Refresh();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tbx_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
